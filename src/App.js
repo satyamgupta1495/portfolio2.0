ed787f25037edb33d98fa3649e093abca0a9f280
Satyam <satyamgupta1495@gmail.com>
2022-07-26 01:31:47 +0530

    version 1.0

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.scss'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout/index.js'
 import Home from './components/Home/index.js'
+import About from './components/About/index.js'
+import Contact from './components/Contact/index.js'
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/portfolio" element={<Portfolio />} /> */}
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
         </Route>
       </Routes>
     </>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,71 @@
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
-function Home() {
-  return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          Hii, <br /> I'm
-          <img src={LogoTitle} alt="developer" />
-          atyam
-          <br />
-          Web Developer
-        </h1>
+import AnimatedLetters from '../AimatedLetters'
+import Logo from './Logo'
+import './index.scss'
+import Loader from 'react-loaders'
 
-        <h2>Frontend Developer / Web3 Noob / Open Source Contributor /</h2>
-        <Link to="/contact" className="flat-button">
-          {' '}
-          CONTACT ME{' '}
-        </Link>
+function Home() {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['a', 't', 'y', 'a', 'm']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  // useEffect(() => {
+  //   return setTimeout(() => {
+  //     console.log('not working')
+  //     setLetterClass('text-animate-hover')
+  //   }, 1000)
+  // }, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+
+          <h2>Frontend Developer / Web3 Noob / Open Source Contributor /</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+          <Logo />
+        </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
